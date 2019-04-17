c3df2211cf8e80708ba5b833666ef194a0521aa3
Cory <cbosselmann1029@gmail.com>
2019-04-17 18:04:57 -0500

    setting up for api calls

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props){
+    super(props)
+
+    this.state = {
+      searchTerm: '',
+    }
+  }
+
+  getSearchTerm=(e)=>{
+    const searchTerm = e.target.value;
+    this.setState({searchTerm: searchTerm})
+  }
+
+  searchRecipe=()=>{
+    //do fetching stuff here
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <input type='text' placeholder="Enter type of recipe"></input>
+          <button onClick={this.getSearchTerm()}>Search</button>
         </header>
       </div>
     );
