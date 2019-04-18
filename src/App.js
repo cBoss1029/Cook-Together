fd63317b71ec5ee0585c1903ab5d0f983362cfeb
Cory <cbosselmann1029@gmail.com>
2019-04-18 16:21:48 -0500

    setting up recipe card component

--- src/App.js
+++ src/App.js
@@ -13,19 +13,21 @@ class App extends Component {
 
   getSearchTerm=(e)=>{
     const searchTerm = e.target.value;
-    this.setState({searchTerm: searchTerm})
+    this.setState({searchTerm: searchTerm});
+    console.log(this.state.searchTerm);
   }
 
   searchRecipe=()=>{
     //do fetching stuff here
+    this.setState({searchTerm: ''});
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <input type='text' placeholder="Enter type of recipe"></input>
-          <button onClick={this.getSearchTerm()}>Search</button>
+          <input onChange={this.getSearchTerm} type='text' value={this.state.searchTerm} placeholder="Enter Search"></input>
+          <button onClick={this.searchRecipe}>Search</button>
         </header>
       </div>
     );
